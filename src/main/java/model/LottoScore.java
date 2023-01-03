fe759a00be42dfad436cde3448511de71beb816f
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-03 11:35:09 +0900

    feat: create LottoPrize, LottoScore

--- src/main/java/model/LottoScore.java
+++ src/main/java/model/LottoScore.java
@@ -7,6 +7,10 @@ public class LottoScore {
     private final boolean isMatchBonus;
 
     public LottoScore(int matchNumber, boolean isMatchBonus) {
+        if (matchNumber < 0 || matchNumber > 6 || (matchNumber == 6 && isMatchBonus)) {
+            throw new IllegalArgumentException();
+        }
+
         this.matchNumber = matchNumber;
         this.isMatchBonus = isMatchBonus;
     }
@@ -15,6 +19,21 @@ public class LottoScore {
         return matchNumber;
     }
 
+    public boolean isMatchBonus() {
+        return isMatchBonus;
+    }
+
+    public boolean compare(LottoScore that) {
+        if (this.matchNumber != that.matchNumber) {
+            return false;
+        }
+
+        if (this.matchNumber != 5) {
+            return true;
+        }
+
+        return this.isMatchBonus == that.isMatchBonus;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -28,7 +47,6 @@ public class LottoScore {
     public int hashCode() {
         return Objects.hash(matchNumber, isMatchBonus);
     }
-
 
     @Override
     public String toString() {
--- src/test/java/model/LottoPrizeTest.java
+++ src/test/java/model/LottoPrizeTest.java
@@ -1,4 +1,94 @@
 package model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LottoPrizeTest {
+    @ParameterizedTest
+    @MethodSource("generatePrizeData")
+    @DisplayName("로또 점수에 따른 올바른 로또 상금 반환")
+    void prizeTest(LottoScore lottoScore, Long expectedPrize) {
+        LottoPrize lottoPrize = new LottoPrize();
+        Long prize = lottoPrize.getPrize(lottoScore);
+
+        assertThat(prize).isEqualTo(expectedPrize);
+    }
+
+    static Stream<Arguments> generatePrizeData() {
+        return Stream.of(
+                Arguments.of(new LottoScore(0, false), 0L),
+                Arguments.of(new LottoScore(1, false), 0L),
+                Arguments.of(new LottoScore(1, true), 0L),
+                Arguments.of(new LottoScore(2, false), 0L),
+                Arguments.of(new LottoScore(2, true), 0L),
+                Arguments.of(new LottoScore(3, false), 5_000L),
+                Arguments.of(new LottoScore(3, true), 5_000L),
+                Arguments.of(new LottoScore(4, false), 50_000L),
+                Arguments.of(new LottoScore(4, true), 50_000L),
+                Arguments.of(new LottoScore(5, false), 1_500_000L),
+                Arguments.of(new LottoScore(5, true), 30_000_000L),
+                Arguments.of(new LottoScore(6, false), 2_000_000_000L)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateFormatPrizesData")
+    void formatPrizesTest(List<LottoScore> lottoScore, String expected) {
+        LottoPrize lottoPrize = new LottoPrize();
+        String result = lottoPrize.formatPrizes(lottoScore);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> generateFormatPrizesData() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(
+                                new LottoScore(0, false)
+                        ),
+                        "3개 일치 (5000원) - 0개\n4개 일치 (50000원) - 0개\n5개 일치 (1500000원) - 0개\n5개 일치, 보너스 볼 일치 (30000000원) - 0개\n6개 일치 (2000000000원) - 0개"
+                ),
+                Arguments.of(
+                        List.of(
+                                new LottoScore(3, false),
+                                new LottoScore(4, false),
+                                new LottoScore(5, false),
+                                new LottoScore(5, true),
+                                new LottoScore(6, false)
+                        ),
+                        "3개 일치 (5000원) - 1개\n4개 일치 (50000원) - 1개\n5개 일치 (1500000원) - 1개\n5개 일치, 보너스 볼 일치 (30000000원) - 1개\n6개 일치 (2000000000원) - 1개"
+                ),
+                Arguments.of(
+                        List.of(
+                                new LottoScore(3, false),
+                                new LottoScore(6, false),
+                                new LottoScore(4, false),
+                                new LottoScore(5, true),
+                                new LottoScore(5, false),
+                                new LottoScore(3, false),
+                                new LottoScore(5, true),
+                                new LottoScore(4, false),
+                                new LottoScore(5, false),
+                                new LottoScore(6, false)
+                        ),
+                        "3개 일치 (5000원) - 2개\n4개 일치 (50000원) - 2개\n5개 일치 (1500000원) - 2개\n5개 일치, 보너스 볼 일치 (30000000원) - 2개\n6개 일치 (2000000000원) - 2개"
+                ),
+                Arguments.of(
+                        List.of(
+                                new LottoScore(0, false),
+                                new LottoScore(1, false),
+                                new LottoScore(2, false),
+                                new LottoScore(3, false)
+                        ),
+                        "3개 일치 (5000원) - 1개\n4개 일치 (50000원) - 0개\n5개 일치 (1500000원) - 0개\n5개 일치, 보너스 볼 일치 (30000000원) - 0개\n6개 일치 (2000000000원) - 0개"
+                )
+        );
+    }
 }
