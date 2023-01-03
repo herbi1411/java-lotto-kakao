254cbe9dfc453bb6b3001922dedb76a44d9dd651
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-03 11:37:12 +0900

    feat: create Controller, View

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,4 +1,39 @@
 package controller;
 
+import model.LottoResult;
+import model.Lottos;
+import model.RandomNumbers;
+import model.WinningLotto;
+import view.InputView;
+import view.OutputView;
+
 public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        long money = inputView.inputMoney();
+        Lottos lottos = new Lottos(new RandomNumbers());
+
+        int times = lottos.calcTimes(money);
+        outputView.putTimes(times);
+
+        lottos.generate(times);
+        outputView.printLottos(lottos);
+
+        String lottoString = inputView.getLottoString();
+        int bonus = inputView.getBonus();
+
+        WinningLotto winningLotto = new WinningLotto(lottoString, bonus);
+        LottoResult lottoResult = new LottoResult(lottos, winningLotto);
+
+        outputView.printResult(lottoResult);
+        outputView.printEarningRate(lottoResult.getEarningRate(money));
+    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,30 @@
 package view;
 
+import model.LottoResult;
+import model.Lottos;
+
 public class OutputView {
+    public void putTimes(int times) {
+        System.out.println(times + "개를 구매했습니다.");
+    }
+
+    public void printLottos(Lottos lottos) {
+        lottos.getLottos().forEach(lotto -> System.out.println(lotto.toString()));
+        System.out.println();
+    }
+
+    public void printResult(LottoResult lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("-------");
+        System.out.println(lottoResult.getResult() + "\n");
+    }
+
+    public void printEarningRate(double earningRate) {
+        System.out.printf("총 수익률은 %.2f입니다.\n", earningRate);
+        if (earningRate < 1.0) {
+            System.out.println("손해!");
+            return;
+        }
+        System.out.println("이득!");
+    }
 }
