bbbd2e421849f293baa3b8189c84475b30ddf1be
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-04 22:18:47 +0900

    fix: 정답 로또 번호의 숫자와 보너스 번호가 중복될 때 예외처리

--- src/main/java/model/WinningLottoSet.java
+++ src/main/java/model/WinningLottoSet.java
@@ -1,5 +1,8 @@
 package model;
 
+import exception.LottoException;
+import exception.LottoExceptionCode;
+
 import java.util.Set;
 
 public class WinningLottoSet {
@@ -9,8 +12,18 @@ public class WinningLottoSet {
 
     public WinningLottoSet(Lotto winningLotto, int bonusNumber) {
 
+        validateWinningLottoSet(winningLotto, bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateWinningLottoSet(Lotto winningLotto, int bonusNumber) {
+        Set<LottoNumber> lottoNumbers = winningLotto.getNumbers();
+        LottoNumber wrappedBonusNumber = LottoNumber.from(bonusNumber);
+
+        if (lottoNumbers.contains(wrappedBonusNumber)) {
+            throw new LottoException(LottoExceptionCode.WINNING_LOTTO_SET_DUPLICATE);
+        }
     }
 
     public LottoScore getScore(Lotto lotto) {
