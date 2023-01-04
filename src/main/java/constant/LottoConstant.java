4185c82d651c03de12bbb248e401cd5b5f5ef2c7
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-04 14:28:44 +0900

    refactor: LottoConstant 주석 추가
    - 사용 영역별로 상수 구분 후 주석을 통해 쉽게 구별할 수 있도록 수정

--- src/main/java/constant/LottoConstant.java
+++ src/main/java/constant/LottoConstant.java
@@ -1,10 +1,12 @@
 package constant;
 
 public class LottoConstant {
+    // 로또 기본 정보 (로또번호 최대값, 최소값, 번호 개수)
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
     public static final int LOTTO_COUNT = 6;
 
+    // 로또 상금
     public static final long LOTTO_1ST_PRIZE = 2_000_000_000L;
     public static final long LOTTO_2ND_PRIZE = 30_000_000L;
     public static final long LOTTO_3RD_PRIZE = 1_500_000L;
@@ -13,5 +15,6 @@ public class LottoConstant {
 
     public static final int LOTTO_TICKET_PRICE = 1000;
 
+    // 로또 번호 집합 input시 delimiter
     public static final String LOTTO_NUMBER_DELIMITER = ", ";
 }
